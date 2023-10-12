ea7a1a0bb4b1c6b90770f4d860c0ffb9130d64fb
GNReplay <giangnguyenreplay@gmail.com>
2023-10-12 19:35:20 +0700

    Update profile

--- app/user/profile-form.tsx
+++ app/user/profile-form.tsx
@@ -26,6 +26,13 @@ import {
 } from "@/components/ui/select"
 import { Textarea } from "@/components/ui/textarea"
 import { toast } from "@/components/ui/use-toast"
+import { useState } from "react"
+
+
+import { IUser } from "@/types"
+import useSWR from "swr";
+
+
 
 const profileFormSchema = z.object({
     username: z
@@ -40,39 +47,31 @@ const profileFormSchema = z.object({
         .string({
             required_error: "Please select an email to display.",
         })
-        .email(),
-    bio: z.string().max(160).min(4),
-    urls: z
-        .array(
-            z.object({
-                value: z.string().url({ message: "Please enter a valid URL." }),
-            })
-        )
-        .optional(),
+        .email()
 })
 
 type ProfileFormValues = z.infer<typeof profileFormSchema>
 
 // This can come from your database or API.
-const defaultValues: Partial<ProfileFormValues> = {
-    bio: "I own a computer.",
-    urls: [
-        { value: "https://shadcn.com" },
-        { value: "http://twitter.com/shadcn" },
-    ],
-}
+// const defaultValues: Partial<ProfileFormValues> = {
+//     bio: "I own a computer.",
+//     urls: [
+//         { value: "https://shadcn.com" },
+//         { value: "http://twitter.com/shadcn" },
+//     ],
+// }
 
 export default function ProfileForm() {
     const form = useForm<ProfileFormValues>({
         resolver: zodResolver(profileFormSchema),
-        defaultValues,
+        //,defaultValues
         mode: "onChange",
     })
 
-    const { fields, append } = useFieldArray({
-        name: "urls",
-        control: form.control,
-    })
+    // const { fields, append } = useFieldArray({
+    //     name: "urls",
+    //     control: form.control,
+    // })
 
     function onSubmit(data: ProfileFormValues) {
         toast({
@@ -85,6 +84,19 @@ export default function ProfileForm() {
         })
     }
 
+    const [isDisable, setIsDisable] = useState(true)
+    const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+    const { data, error, isLoading } = useSWR(
+        "https://dummyjson.com/users/1",
+        fetcher,
+        {
+            revalidateIfStale: false,
+            revalidateOnFocus: false,
+            revalidateOnReconnect: false
+        }
+    );
+
     return (
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -95,7 +107,7 @@ export default function ProfileForm() {
                         <FormItem>
                             <FormLabel>Username</FormLabel>
                             <FormControl>
-                                <Input placeholder="shadcn" {...field} />
+                                <Input value={data?.firstName + " " + data?.lastName} disabled={isDisable} />
                             </FormControl>
                             <FormDescription>
                                 This is your public display name. It can be your real name or a
@@ -111,18 +123,9 @@ export default function ProfileForm() {
                     render={({ field }) => (
                         <FormItem>
                             <FormLabel>Email</FormLabel>
-                            <Select onValueChange={field.onChange} defaultValue={field.value}>
-                                <FormControl>
-                                    <SelectTrigger>
-                                        <SelectValue placeholder="Select a verified email to display" />
-                                    </SelectTrigger>
-                                </FormControl>
-                                <SelectContent>
-                                    <SelectItem value="m@example.com">m@example.com</SelectItem>
-                                    <SelectItem value="m@google.com">m@google.com</SelectItem>
-                                    <SelectItem value="m@support.com">m@support.com</SelectItem>
-                                </SelectContent>
-                            </Select>
+                            <FormControl>
+                                <Input value={data?.email} disabled={isDisable} />
+                            </FormControl>
                             <FormDescription>
                                 You can manage verified email addresses in your{" "}
                                 <Link href="/examples/forms">email settings</Link>.
@@ -131,7 +134,7 @@ export default function ProfileForm() {
                         </FormItem>
                     )}
                 />
-                <FormField
+                {/* <FormField
                     control={form.control}
                     name="bio"
                     render={({ field }) => (
@@ -183,9 +186,15 @@ export default function ProfileForm() {
                     >
                         Add URL
                     </Button>
+                </div> */}
+                <div className="grid w-1/2 grid-cols-2 gap-10">
+                    <Button type="button" className="flex" onClick={() => setIsDisable(false)}>Update profile</Button>
+                    <Button className="flex disabled:bg-slate-200 disabled:cursor-not-allowed" type="submit" disabled={isDisable} >Save</Button>
                 </div>
-                <Button type="submit">Update profile</Button>
+
             </form>
+
+
         </Form>
     )
 }
--- types.d.ts
+++ types.d.ts
@@ -7,3 +7,15 @@ export type course = {
   price: string;
   oldPrice: string;
 };
+
+export type IUser = {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  image: string;
+  role: string;
+  activation_code: string;
+  registration_date: string;
+  status: string;
+}
